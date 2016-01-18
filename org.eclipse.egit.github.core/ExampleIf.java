732be36f21314f2a40def3cfe03c1c6116d6a25c
Olga Hollister <olya778899@gmail.com>
2016-01-18 12:35:22 -0500

    Create ExampleIf.java

---
+++ org.eclipse.egit.github.core/ExampleIf.java
@@ -0,0 +1,23 @@
+
+
+public class ExampleIf {
+
+	public static void main(String[] args) {
+		
+        int x = 20;
+        int y = 20;
+         
+        if (x<y){
+        	 System.out.println("x is less than y");
+        	 }
+        else if(x>y){
+        		 System.out.println("x is greater than y");
+        		 }
+        else if(x==y){
+        			 System.out.println("x is equal to y");
+        		 }
+        	 
+         }
+	
+
+}
